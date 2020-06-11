713469fc9742ad7dd9bd4efeb041302e9d872df2
Arya <sailesharya12@gmail.com>
2020-06-11 17:57:45 +0530

    final code after removing unwanted services

--- config.js
+++ config.js
@@ -1,19 +1,4 @@
 module.exports = {
-    dbPool: {
-        user: "DFTE_DSPEC_APPS",
-        password: 'Dspec#123', 
-        connectString: "150.136.240.142/dftedev_pdb1.dftepublicsubne.dftevcn.oraclevcn.com",
-        poolMin: 32,
-        poolMax: 32,
-        poolIncrement: 0,
-        poolTimeout:0
-    },
-	networkDrivePathIn : "\\\\usazuconde00173\\DFTE\\R2\\DSPEC\\IN",
-    networkDrivePathOut : "\\\\usazuconde00173\\DFTE\\R2\\DSPEC\\OUT",
-	repo : {
-		owner : "sailesharya",
-		repo : "dspec-repo",
-		refFile : "DSPEC_Report_Keywords2.csv",
-		accessToken : "beb5181984dddb74623c50dba0f2ee46e70757c6"		
-	}
+    networkDrivePathIn : "\\\\usazuconde00173\\DFTE\\R2\\DSPEC\\IN",
+    networkDrivePathOut : "\\\\usazuconde00173\\DFTE\\R2\\DSPEC\\OUT"
 };
--- routes/file.js
+++ routes/file.js
@@ -1,26 +1,77 @@
-var express = require('express');
-var router = express.Router();
-const FileServices = require('../services/file')
-let networkDrive = require('windows-network-drive');
+const express = require('express');
+const fs = require('fs');
+const router = express.Router();
+const networkDrive = require('windows-network-drive');
 const configData = require('../config');
+const networkDrivePathIn = configData.networkDrivePathIn;
 const networkDrivePathOut = configData.networkDrivePathOut;
 
-router.post('/upload', function (req, res, next) {
-    FileServices.fileUpload(req)
-    .then(data => res.status(200).json(data))
-    .catch(err => {
-        console.log('github search error',err)
-        res.status(400).json(err)
-    })
+networkDrive.find(networkDrivePathIn)
+    .then(function (result) {
+        console.log('Network Drives :',result)
 });
 
-router.get('/download/:filename', function (req, res) {
+var store = multer.diskStorage({
+    destination:function(req,file,callback){
+        console.log('destination');
+        console.log(file);
+		callback(null, './uploads');
+    },
+    filename:function(req,file,callback){
+        callback(null, Date.now()+'.'+file.originalname);
+    }
+});
+
+var upload = multer({storage:store}).single('file');
+
+router.post('/upload', function(req,res,next){
+    upload(req,res,function(err){
+        if(err){
+            return res.status(501).json({error:err});
+        }
+        if(req.file){
+            var file = req.file,
+                name = file.originalname,
+                type = file.mimetype;
+              
+            networkDrive.pathToWindowsPath(networkDrivePathIn)
+				.then(function (windowsPath) {
+					var uploadpath = windowsPath + '\\' + name;
+					console.log(uploadpath);
+					mv(file.path,uploadpath,function(err){
+						if(err)
+							console.log("File Upload Failed for ",uploadpath,err);
+						else
+							console.log("File Uploaded Successfully to ",uploadpath);
+					});
+            });
+       }
+        return res.status(200).json({originalname:req.file.originalname, uploadname:req.file.filename});
+    });
+});
+
+router.get('/download/:userRequestId/:filename', function (req, res) {
     console.log('File Download from network drive for ', req.params.filename)
     networkDrive.pathToWindowsPath(networkDrivePathOut)
         .then(function (windowsPath) {
-            var filePath = windowsPath + '\\' + req.params.filename;
+            var filePath = windowsPath + '\\' + req.params.userRequestId + '\\' + req.params.filename;
             console.log('file downloading path ', filePath);
             res.download(filePath);
+        });
+});
+
+router.get('/delete/:filename', function (req, res) {
+    console.log('File Delete from network drive for ', req.params.filename)
+    networkDrive.pathToWindowsPath(networkDrivePathIn)
+        .then(function (windowsPath) {
+            var filePath = windowsPath + '\\' + req.params.filename;
+            try {
+			  fs.unlinkSync(filePath)
+			  console.log("Successfully deleted the file.")
+			  res.status(200).json({filename:filePath})
+			} catch(err) {
+			  res.status(400).json(err)
+			}
         });
 });
 
